b44ac94048b0b63980cdbf78fa456b8d288206a9
Oliver Q.R. <qvdoli@gmail.com>
2024-10-14 14:34:54 +0200

    Updated gallery

--- slideshow_script.js
+++ slideshow_script.js
@@ -34,3 +34,17 @@ function showSlides(n) {
     }
     slides[slideIndex].style.display = "block";
 }
+
+
+function expandImage(img) {
+    var fullscreenContainer = document.getElementById("full-screen-image-container");
+    var fullscreenImg = document.getElementById("full-screen-image");
+
+    fullscreenImg.src = img.src;
+    fullscreenContainer.style.display = "flex";
+}
+
+function closeExpandedImage() {
+    var fullscreenContainer = document.getElementById("full-screen-image-container");
+    fullscreenContainer.style.display = "none";
+}
