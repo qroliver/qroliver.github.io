c81be09b8d812dc8e2417806ff641240fe18eeeb
qroliver <oqr@oliverqr.com>
2025-04-21 14:14:24 +0200

    Font update

---
+++ footer_year_update.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+        const currentYear = new Date().getFullYear();
+        yearSpan.textContent = currentYear;
+    }
+});
