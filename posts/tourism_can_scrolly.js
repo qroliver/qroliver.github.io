71d2385b132a510a8ab202ace46d2fc1546be76e
qroliver <oqr@oliverqr.com>
2025-04-25 09:33:10 +0200

    New blog post - Minor tweaks

---
+++ posts/tourism_can_scrolly.js
@@ -0,0 +1,18 @@
+const scenes = document.querySelectorAll(".scene");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  },
+  {
+    threshold: 0.2, 
+  }
+);
+
+scenes.forEach(scene => observer.observe(scene));
