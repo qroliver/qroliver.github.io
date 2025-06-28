d8222073faff0b57d9bd647cb7a137f156bb171e
qroliver <oqr@oliverqr.com>
2025-06-28 09:37:17 +0200

    Minor fix

--- js/ios_pdf.js
+++ js/ios_pdf.js
@@ -4,6 +4,6 @@ if (isMobile) {
     const container = document.getElementById('pdf-container');
     container.innerHTML = `
     <p>PDF is not supported on your device.
-    <a href="/posts/img/lzt_net_zero_plan_img/lzt_net-zero.pdf" download>Click here to download the PDF</a></p>
+    <a href="/posts/img/lzt_net_zero_plan_img/lzt_net-zero.pdf" style='color:rgb(12, 102, 117);' download>Click here to download the PDF</a></p>
     `;
 }
